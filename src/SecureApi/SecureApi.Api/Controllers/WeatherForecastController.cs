58952d39e9ee8cf4f298b3a25618849b4d2875d6
Jan de Vries <jan@jan-v.nl>
2020-04-06 21:56:28 +0200

    Creating an authenticated weather request

--- src/SecureApi/SecureApi.Api/Controllers/WeatherForecastController.cs
+++ src/SecureApi/SecureApi.Api/Controllers/WeatherForecastController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Logging;
 
 namespace SecureApi.Api.Controllers
@@ -15,25 +18,38 @@ namespace SecureApi.Api.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        private readonly IHttpClientFactory clientFactory;
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            IHttpClientFactory clientFactory,
+            ILogger<WeatherForecastController> logger)
         {
+            this.clientFactory = clientFactory;
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public class ApiCallDetails
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            public string AccessToken { get; set; }
+            public string Body { get; set; }
+        }
+        [HttpGet]
+        public async Task<ApiCallDetails> Get()
+        {
+            const string applicationIdUri = "api://6d7649c2-de4f-4ce4-83f5-422d4f6c5fe0";
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync(applicationIdUri);
+            var httpClient = clientFactory.CreateClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await httpClient.GetAsync("https://janv-secureapi-speakers.azurewebsites.net/WeatherForecast");
+            var body = await response.Content.ReadAsStringAsync();
+
+            return new ApiCallDetails
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                AccessToken = accessToken,
+                Body = body
+            };
         }
     }
 }
--- src/SecureApi/SecureApi.Api/Startup.cs
+++ src/SecureApi/SecureApi.Api/Startup.cs
@@ -26,6 +26,7 @@ namespace SecureApi.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
