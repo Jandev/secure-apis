6db38e0f057c7b59803ee8ca69f3f1b83a82669a
Jan de Vries <jan@jan-v.nl>
2020-04-28 20:59:12 +0200

    Added warmup controller with delay

---
+++ src/SecureApi/SecureApi.Api/Controllers/WarmupController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace SecureApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarmupController : ControllerBase
+    {
+        private readonly ILogger<WarmupController> logger;
+
+        public WarmupController(ILogger<WarmupController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            this.logger.LogInformation($"Executing {nameof(WarmupController)}.{nameof(Get)}");
+
+            await Task.Delay(5000);
+
+            this.logger.LogInformation($"Executed {nameof(WarmupController)}.{nameof(Get)}");
+            return Ok();
+        }
+    }
+}
