e754445e244d4f5fc57d3fff68d6b32866945c1d
Jan de Vries <jan@jan-v.nl>
2020-05-11 21:40:54 +0200

    Added new endpoint for conferences

---
+++ src/SecureApi/SecureApi.Conferences/Controllers/BigConferencesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace SecureApi.Conferences.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BigConferencesController : ControllerBase
+    {
+        private readonly ILogger<BigConferencesController> logger;
+
+        public BigConferencesController(ILogger<BigConferencesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Conference> Get()
+        {
+            var conferences = new List<Conference>();
+
+            conferences.AddRange(new []{
+                new Conference
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "NDC Oslo"
+                },
+                new Conference
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "NDC Sydney"
+                },
+                new Conference
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Techorama"
+                }});
+
+            return conferences;
+        }
+    }
+
+    public class Conference
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
