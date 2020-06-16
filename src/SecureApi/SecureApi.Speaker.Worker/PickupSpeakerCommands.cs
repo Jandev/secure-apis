7fdffa9d44e1330b52367b8cf3cc62ce60d045d3
Jan de Vries <jan@jan-v.nl>
2020-06-16 20:20:11 +0200

    Empty function for add speaker command

--- src/SecureApi/SecureApi.Speaker.Worker/PickupSpeakerCommands.cs
+++ src/SecureApi/SecureApi.Speaker.Worker/PickupSpeakerCommands.cs
@@ -1,17 +1,53 @@
+using AzureFunctions.EventGrid;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using SecureApi.Domain.Contracts.Commands;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace SecureApi.Speaker.Worker
 {
-    public static class PickupSpeakerCommands
+    public class PickupSpeakerCommands
     {
         [FunctionName(nameof(PickupSpeakerCommands))]
-        public static void Run(
+        public async Task Run(
             [QueueTrigger("speaker-commands", Connection = "Speakers:StorageAccount")]
-            string myQueueItem, 
+            string command,
+            [EventGrid(
+                // The endpoint of your Event Grid Topic, this should be specified in your application settings of the Function App
+                TopicEndpoint = "EventGridBindingSampleTopicEndpoint", 
+                // This is the secret key to connect to your Event Grid Topic. To be placed in the application settings.
+                TopicKey = "EventGridBindingSampleTopicKey")]
+            IAsyncCollector<Event> outputCollector,
             ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            log.LogInformation($"Executing {nameof(PickupSpeakerCommands)}");
+
+            var deserializedCommand = JsonSerializer.Deserialize<AddSpeaker>(command);
+
+            // Domain logic over here...
+
+            await EmitEvent(outputCollector, deserializedCommand);
+        }
+
+        private static async Task EmitEvent(IAsyncCollector<Event> outputCollector, AddSpeaker deserializedCommand)
+        {
+            // Specify some meta data of the message you want to publish to Event Grid
+            var myTestEvent = new Event
+            {
+                EventType = "SecureApi.Speaker.SpeakerAdded",
+                Subject = "SpeakerAdded",
+                Data = new
+                {
+                    deserializedCommand.Id,
+                    deserializedCommand.FirstName,
+                    deserializedCommand.LastName
+                },
+                DataVersion = "0.1"
+            };
+
+            // Add the event to the IAsyncCollector<T> in order to get your event published.
+            await outputCollector.AddAsync(myTestEvent);
         }
     }
 }
